d11e90165a2379cd69af65605a82adbfba44148e
nyirurugo omar <nyirurugoomar@gmail.com>
2023-11-21 09:28:11 +0200

    addding user Authentication api

--- server/index.js
+++ server/index.js
@@ -30,5 +30,5 @@ app.use(router);
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { getCards,createCard,updateCard, deleteCard } = require('./controllers/Card');
+const { register, login } = require('./controllers/authController.js');
 
 
 
@@ -7,6 +8,8 @@ router.get("/cards", getCards);
 router.post('/cards',createCard)
 router.put('/cards/:cardID',updateCard)
 router.delete('/cards/:cardID',deleteCard)
+router.post('/register', register);
+router.post('/login', login);
 
 
 module.exports = router;
